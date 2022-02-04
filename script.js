541d4b0776251b5f56860fa90f59745b9aa1a943
satorina-vDev <nonato.miau@hotmail.com>
2022-02-04 03:08:08 -0300

    trab4

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+//#1
+function comversa() {
+    console.log("ola senho kirito seja bem vindo a sao");
+}
+  
+comversa();
+
+//#2
+function comversas(nome) {
+    console.log(`Olá ${nome}, seja bem vindo(a) ao mundo de sao`);
+}
+
+comversas("Kirito"); 
+
+//#3
+
+function dados(nome, numero, musica) {
+    console.log(`seu nome e ${nome},seu numero e ${numero}, sua musica e ${musica}`);
+}
+
+dados("kirito",127,"pagode");
+
+//#4
+function dadoss(filme, musica) {
+    console.log(`seu nome e ${filme},sua musica e ${musica}`);
+}
+
+  dadoss("soword art oline","pagode Russo");
+
+//#5
+
+
+//function comversas(nome) {
+    //console.log(${nome = nome . 3});
+//}
+
+//comversas(4); 
+
